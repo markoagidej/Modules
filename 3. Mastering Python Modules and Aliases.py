dbc6afdec3bc3d0601fe2f9632397939b56f4ae0
Marko <markogidej@yahoo.com>
2024-04-16 12:13:04 -0400

    Assignment 3 done

--- 3. Mastering Python Modules and Aliases.py
+++ 3. Mastering Python Modules and Aliases.py
@@ -1,1 +1,21 @@
 # Task 1: Custom Module with Aliases
+
+import text_utils as txt_util
+
+my_string = input("Input a string!\n")
+
+print("What would you liek to do with this string?")
+print("1. Reverse the string")
+print("2. Capitalize the string")
+
+choice = input()
+
+if choice == "1":
+    new_string = txt_util.reverse_string(my_string)
+elif choice == "2":
+    new_string = txt_util.capitalize_string(my_string)
+else:
+    print("Choose 1 or 2!")
+    exit()
+
+print(f"Here is your new string:\n{new_string}")
---
+++ text_utils.py
@@ -0,0 +1,7 @@
+
+def reverse_string(string):
+    return string[::-1]
+
+
+def capitalize_string(string):
+    return string.upper()
